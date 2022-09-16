6793bdf9d16b0244ced8d4592e5f7818a3b31b30
FPNBR <felipepny@gmail.com>
2022-09-16 15:44:39 -0300

    Innitial Commit

---
+++ src/program/Main.java
@@ -0,0 +1,16 @@
+package program;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World");
+
+        sc.close();
+    }
+}
