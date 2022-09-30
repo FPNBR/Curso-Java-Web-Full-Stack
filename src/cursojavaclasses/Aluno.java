8a19d4a298962ba5a184da0c45a0be279a98833b
FPNBR <felipepny@gmail.com>
2022-09-29 21:30:12 -0300

    Primeira classe e objeto aluno

---
+++ src/cursojavaclasses/Aluno.java
@@ -0,0 +1,14 @@
+package cursojavaclasses;
+
+public class Aluno {
+    String nome;
+    int idade;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+    String nomeMae;
+    String nomePai;
+    String nomeMatricula;
+    String nomeEscola;
+    String serieMatriculado;
+}
---
+++ src/cursojavaexecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,4 @@
+package cursojavaexecutavel;
+
+public class PrimeiraClasseJava {
+}
