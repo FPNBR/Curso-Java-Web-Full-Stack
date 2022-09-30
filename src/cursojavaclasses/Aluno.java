52dd06563ed7099e9ce2b79a7f4d4fb50237719c
FPNBR <felipepny@gmail.com>
2022-09-29 22:04:13 -0300

    Sobre o construtor do objeto

--- src/cursojavaclasses/Aluno.java
+++ src/cursojavaclasses/Aluno.java
@@ -11,4 +11,16 @@ public class Aluno {
     String nomeMatricula;
     String nomeEscola;
     String serieMatriculado;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nomePadrao) {
+        nome = nomePadrao;
+    }
+
+    public Aluno(String nomePadrao, int idadePadrao) {
+        nome = nomePadrao;
+        idade = idadePadrao;
+    }
 }
--- src/cursojavaexecutavel/PrimeiraClasseJava.java
+++ src/cursojavaexecutavel/PrimeiraClasseJava.java
@@ -1,4 +1,18 @@
 package cursojavaexecutavel;
 
+import cursojavaclasses.Aluno;
+
 public class PrimeiraClasseJava {
+
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+
+        Aluno aluno2 = new Aluno();
+
+        Aluno aluno3 = new Aluno();
+
+        Aluno aluno4 = new Aluno("Maria");
+
+        Aluno aluno5 = new Aluno("José", 50 );
+    }
 }
