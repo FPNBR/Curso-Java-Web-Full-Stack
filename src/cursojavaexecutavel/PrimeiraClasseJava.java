ab4faa38267bc4de6b47d37db8d5a16c72008afb
FPNBR <felipepny@gmail.com>
2022-09-30 00:12:40 -0300

    Método de cálculo de média de nota

--- src/cursojavaexecutavel/PrimeiraClasseJava.java
+++ src/cursojavaexecutavel/PrimeiraClasseJava.java
@@ -5,6 +5,7 @@ import cursojavaclasses.Aluno;
 public class PrimeiraClasseJava {
 
     public static void main(String[] args) {
+
         Aluno aluno1 = new Aluno();
         aluno1.setNome("João da Silva");
         aluno1.setIdade(50);
@@ -16,10 +17,15 @@ public class PrimeiraClasseJava {
         aluno1.setDataMatricula("10/01/2019");
         aluno1.setNomeEscola("Escola JDEV Treinamento");
         aluno1.setSerieMatriculado("5");
+        aluno1.setNota1(90);
+        aluno1.setNota2(80.8);
+        aluno1.setNota3(70.9);
+        aluno1.setNota4(90.7);
 
         System.out.println("O nome do aluno é = " + aluno1.getNome());
         System.out.println("A idade do aluno é = " + aluno1.getIdade());
         System.out.println("A data de nascimento do aluno é = " + aluno1.getDataNascimento());
+        System.out.println("A média da nota do aluno é = " + aluno1.getMediaNota());
         System.out.println("----------------------------------------");
 
         Aluno aluno2 = new Aluno();
