37be96ea50328a2c3a9b0156227af909080c62e6
FPNBR <felipepny@gmail.com>
2022-10-05 01:48:28 -0300

    Criando a lista dinâmica das disciplinas do aluno

--- src/cursojavaexecutavel/PrimeiraClasseJava.java
+++ src/cursojavaexecutavel/PrimeiraClasseJava.java
@@ -32,26 +32,14 @@ public class PrimeiraClasseJava {
         aluno1.setSerieMatriculado(serie);
         aluno1.setNomeEscola(nomeEscola);
 
-        Disciplina disciplina1 = new Disciplina();
-        disciplina1.setDisciplina("Banco de dados");
-        disciplina1.setNota(90);
-
-        Disciplina disciplina2 = new Disciplina();
-        disciplina2.setDisciplina("Matemática");
-        disciplina2.setNota(80);
-
-        Disciplina disciplina3 = new Disciplina();
-        disciplina3.setDisciplina("Geografia");
-        disciplina3.setNota(70);
-
-        Disciplina disciplina4 = new Disciplina();
-        disciplina4.setDisciplina("Java Web");
-        disciplina4.setNota(70);
-
-        aluno1.getDisciplinas().add(disciplina1);
-        aluno1.getDisciplinas().add(disciplina2);
-        aluno1.getDisciplinas().add(disciplina3);
-        aluno1.getDisciplinas().add(disciplina4);
+        for (int i = 1; i <= 4; i++) {
+            String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+i+" ?");
+            String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina "+i+" ?");
+            Disciplina disciplina = new Disciplina();
+            disciplina.setDisciplina(nomeDisciplina);
+            disciplina.setNota(Double.parseDouble(notaDisciplina));
+            aluno1.getDisciplinas().add(disciplina);
+        }
 
         System.out.println(aluno1);
         System.out.println("A média da nota do aluno é = " + aluno1.getMediaNota());
