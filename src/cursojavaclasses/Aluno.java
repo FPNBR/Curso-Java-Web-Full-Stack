299cbe773f4ee17cf7034744ca73b5289a847af1
FPNBR <felipepny@gmail.com>
2022-10-04 23:39:54 -0300

    Criando atributos de disciplina do aluno

--- src/cursojavaclasses/Aluno.java
+++ src/cursojavaclasses/Aluno.java
@@ -14,9 +14,13 @@ public class Aluno {
     private String nomeEscola;
     private String serieMatriculado;
     private double nota1;
+    private String disciplina1;
     private double nota2;
+    private String disciplina2;
     private double nota3;
+    private String disciplina3;
     private double nota4;
+    private String disciplina4;
 
     public Aluno() {
     }
@@ -142,6 +146,38 @@ public class Aluno {
         this.nota4 = nota4;
     }
 
+    public String getDisciplina1() {
+        return disciplina1;
+    }
+
+    public void setDisciplina1(String disciplina1) {
+        this.disciplina1 = disciplina1;
+    }
+
+    public String getDisciplina2() {
+        return disciplina2;
+    }
+
+    public void setDisciplina2(String disciplina2) {
+        this.disciplina2 = disciplina2;
+    }
+
+    public String getDisciplina3() {
+        return disciplina3;
+    }
+
+    public void setDisciplina3(String disciplina3) {
+        this.disciplina3 = disciplina3;
+    }
+
+    public String getDisciplina4() {
+        return disciplina4;
+    }
+
+    public void setDisciplina4(String disciplina4) {
+        this.disciplina4 = disciplina4;
+    }
+
     public double getMediaNota() {
         return (nota1 + nota2 + nota3 + nota4) / 4.0;
     }
@@ -180,9 +216,13 @@ public class Aluno {
                 ", nomeEscola='" + nomeEscola + '\'' +
                 ", serieMatriculado='" + serieMatriculado + '\'' +
                 ", nota1=" + nota1 +
+                ", disciplina1='" + disciplina1 + '\'' +
                 ", nota2=" + nota2 +
+                ", disciplina2='" + disciplina2 + '\'' +
                 ", nota3=" + nota3 +
+                ", disciplina3='" + disciplina3 + '\'' +
                 ", nota4=" + nota4 +
+                ", disciplina4='" + disciplina4 + '\'' +
                 '}';
     }
 
--- src/cursojavaexecutavel/PrimeiraClasseJava.java
+++ src/cursojavaexecutavel/PrimeiraClasseJava.java
@@ -8,7 +8,7 @@ public class PrimeiraClasseJava {
 
     public static void main(String[] args) {
 
-        /*String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+        String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
         String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
         String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno?");
         String rg = JOptionPane.showInputDialog("Qual o RG do aluno?");
@@ -18,9 +18,13 @@ public class PrimeiraClasseJava {
         String dataMatricula = JOptionPane.showInputDialog("Qual a data de matrícula do aluno?");
         String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola do aluno?");
         String serie = JOptionPane.showInputDialog("Qual a série do aluno?");
+        String disciplinaUm = JOptionPane.showInputDialog("Qual a disciplina 1 do aluno?");
         String notaUm = JOptionPane.showInputDialog("Digite a nota 1 do aluno");
+        String disciplinaDois = JOptionPane.showInputDialog("Qual a disciplina 2 do aluno?");
         String notaDois = JOptionPane.showInputDialog("Digite a nota 2 do aluno");
+        String disciplinaTres = JOptionPane.showInputDialog("Qual a disciplina 3 do aluno?");
         String notaTres = JOptionPane.showInputDialog("Digite a nota 3 do aluno");
+        String disciplinaQuatro = JOptionPane.showInputDialog("Qual a disciplina 4 do aluno?");
         String notaQuatro = JOptionPane.showInputDialog("Digite a nota 4 do aluno");
 
         Aluno aluno1 = new Aluno();
@@ -34,30 +38,19 @@ public class PrimeiraClasseJava {
         aluno1.setDataMatricula(dataMatricula);
         aluno1.setNomeEscola(nomeEscola);
         aluno1.setSerieMatriculado(serie);
+        aluno1.setDisciplina1(disciplinaUm);
         aluno1.setNota1(Double.parseDouble(notaUm));
+        aluno1.setDisciplina2(disciplinaDois);
         aluno1.setNota2(Double.parseDouble(notaDois));
+        aluno1.setDisciplina3(disciplinaTres);
         aluno1.setNota3(Double.parseDouble(notaTres));
+        aluno1.setDisciplina4(disciplinaQuatro);
         aluno1.setNota4(Double.parseDouble(notaQuatro));
 
         System.out.println(aluno1);
         System.out.println("A média da nota do aluno é = " + aluno1.getMediaNota());
         System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-        System.out.println("Resultado 2 = " + (aluno1.getAlunoAprovado2()));*/
-
-        Aluno aluno1 = new Aluno();
-        aluno1.setNome("Felipe");
-        aluno1.setNumeroCpf("123");
-
-        Aluno aluno2 = new Aluno();
-        aluno2.setNome("Felipe");
-        aluno2.setNumeroCpf("1234");
-
-        if (aluno1.equals(aluno2)) {
-            System.out.println("Alunos são iguais");
-        }
-        else {
-            System.out.println("Alunos não são iguais");
-        }
+        System.out.println("Resultado 2 = " + (aluno1.getAlunoAprovado2()));
 
     }
 }
