d728dc865b9e8381d0d1b9c9168f1c8bb9b87755
FPNBR <felipepny@gmail.com>
2022-10-05 02:15:02 -0300

    Método se o aluno está em recuperação

--- src/cursojavaclasses/Aluno.java
+++ src/cursojavaclasses/Aluno.java
@@ -140,8 +140,13 @@ public class Aluno {
 
     public String getAlunoAprovado2() {
         double media = this.getMediaNota();
-        if (media >= 70) {
-            return "Aluno está aprovado";
+        if (media >= 50) {
+            if (media >= 70) {
+                return "Aluno está aprovado";
+            }
+            else {
+                return "Aluno em recuperação";
+            }
         }
         else {
             return "Aluno está reprovado";
