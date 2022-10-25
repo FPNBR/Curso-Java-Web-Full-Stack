c28c706d95e0d0840094ec5aa30f32ac3e3f13da
FPNBR <felipepny@gmail.com>
2022-10-25 13:26:42 -0300

    Criando o primeiro Array

---
+++ src/cursojavaexecutavel/ArrayVetor.java
@@ -0,0 +1,14 @@
+package cursojavaexecutavel;
+
+public class ArrayVetor {
+    public static void main(String[] args) {
+
+        double[] notas = new double[5];
+        notas[0] = 9.8;
+        notas[1] = 8.7;
+        notas[2] = 9.7;
+        notas[3] = 9.9;
+
+        System.out.println(notas);
+    }
+}
