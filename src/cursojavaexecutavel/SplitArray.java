ae8d2fffe723eaa0eb6fd529ea0577ad9aad6108
FPNBR <felipepny@gmail.com>
2022-10-26 17:20:10 -0300

    String e o Método Split para Array

---
+++ src/cursojavaexecutavel/SplitArray.java
@@ -0,0 +1,16 @@
+package cursojavaexecutavel;
+
+public class SplitArray {
+    public static void main(String[] args) {
+
+        String texto = "Felipe, Java, 80, 70, 90, 89";
+        String[] valoresArray = texto.split(",");
+
+        System.out.println("Nome: " + valoresArray[0]);
+        System.out.println("Curso:" + valoresArray[1]);
+        System.out.println("Nota 1:" + valoresArray[2]);
+        System.out.println("Nota 2:" + valoresArray[3]);
+        System.out.println("Nota 3:" + valoresArray[4]);
+        System.out.println("Nota 4:" + valoresArray[5]);
+    }
+}
