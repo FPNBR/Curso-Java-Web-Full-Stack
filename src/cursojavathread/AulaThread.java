0ee727d22ff253da5b5823e0eb7de4e859a30649
Felipe Pinheiro Nery <felipepny@gmail.com>
2022-10-27 15:43:46 -0300

    Criando a primeira Thread

--- src/cursojavathread/AulaThread.java
+++ src/cursojavathread/AulaThread.java
@@ -1,13 +1,29 @@
 package cursojavathread;
 
+import javax.swing.*;
+import java.lang.Thread;
+
 public class AulaThread {
     public static void main(String[] args ) throws InterruptedException {
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Executando...");
+        new Thread() {
 
-            Thread.sleep(1000);
-        }
-        System.out.println("Execução Finalizada!");
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Executando...");
+
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }.start();
+
+        System.out.println("Teste de Execução da Thread Finalizada!");
+        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
 }
