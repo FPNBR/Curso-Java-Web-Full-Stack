d6f57dbf0733f02db3d571ab66345a80c40df38c
FPNBR <felipepny@gmail.com>
2022-10-28 20:52:13 -0300

    GridBagConstraints gerenciador de posicionamento e layout

--- src/cursojavathread/ExecutarTela.java
+++ src/cursojavathread/ExecutarTela.java
@@ -1,5 +1,8 @@
 package cursojavathread;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ExecutarTela {
     public static void main(String[] args) {
         TelaTimeThread telaTimeThread = new TelaTimeThread();
--- src/cursojavathread/TelaTimeThread.java
+++ src/cursojavathread/TelaTimeThread.java
@@ -4,15 +4,42 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TelaTimeThread extends JDialog {
+
+    private JPanel jPanel = new JPanel(new GridBagLayout());
+    private JLabel descricaoHora = new JLabel("Time Thread 1");
+    private JTextField mostraTempo = new JTextField();
+    private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+    private JTextField mostraTempo2 = new JTextField();
+
     TelaTimeThread() {
-        //Configurações da tela
+        /*Controlador de posicionamento de componentes*/
         setTitle("Tela de Time com Thread");
         setSize(new Dimension(240, 240));
         setLocationRelativeTo(null);
         setResizable(false);
 
 
+        /*Controlador de posicionamento de componentes*/
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
 
+        descricaoHora.setPreferredSize(new Dimension(200, 25));
+        jPanel.add(descricaoHora, gridBagConstraints);
+
+        mostraTempo.setPreferredSize(new Dimension(200, 25));
+        gridBagConstraints.gridy ++;
+        jPanel.add(mostraTempo, gridBagConstraints);
+
+        descricaoHora2.setPreferredSize(new Dimension(200, 25));
+        gridBagConstraints.gridy ++;
+        jPanel.add(descricaoHora2, gridBagConstraints);
+
+        mostraTempo2.setPreferredSize(new Dimension(200, 25));
+        gridBagConstraints.gridy ++;
+        jPanel.add(mostraTempo2, gridBagConstraints);
+
+        add(jPanel, BorderLayout.WEST);
         setVisible(true);
     }
 }
