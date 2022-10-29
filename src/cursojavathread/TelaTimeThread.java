e10a78976fc87fd7812891b572efbdfd14ff8a23
FPNBR <felipepny@gmail.com>
2022-10-28 22:01:37 -0300

    Controlando a Thead 2 com Start e Stop

--- src/cursojavathread/TelaTimeThread.java
+++ src/cursojavathread/TelaTimeThread.java
@@ -31,7 +31,22 @@ public class TelaTimeThread extends JDialog {
         }
     };
 
+    private Runnable thread2 = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                mostraTempo2.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    };
+
     private Thread thread1Time;
+    private Thread thread2Time;
 
     TelaTimeThread() {
         /*Controlador de posicionamento de componentes*/
@@ -80,6 +95,12 @@ public class TelaTimeThread extends JDialog {
             public void actionPerformed(ActionEvent e) {
                 thread1Time = new Thread(thread1);
                 thread1Time.start();
+
+                thread2Time = new Thread(thread2);
+                thread2Time.start();
+
+                jButton.setEnabled(false);
+                jButton2.setEnabled(true);
             }
         });
 
@@ -87,8 +108,14 @@ public class TelaTimeThread extends JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 thread1Time.stop();
+                thread2Time.stop();
+
+                jButton.setEnabled(true);
+                jButton2.setEnabled(false);
             }
         });
+
+        jButton2.setEnabled(false);
 
         add(jPanel, BorderLayout.WEST);
         setVisible(true);
