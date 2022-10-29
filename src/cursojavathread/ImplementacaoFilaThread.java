c60af6b24fec302cff0de9a4c320af7a13dc55e4
FPNBR <felipepny@gmail.com>
2022-10-29 00:19:36 -0300

    Criando a Fila e Pilha para processar paralelo em Thread

---
+++ src/cursojavathread/ImplementacaoFilaThread.java
@@ -0,0 +1,48 @@
+package cursojavathread;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ImplementacaoFilaThread extends Thread {
+
+    private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_fila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+
+    public static void add(ObjetoFilaThread objetoFilaThread) {
+        pilha_fila.add(objetoFilaThread);
+    }
+
+    @Override
+    public void run() {
+        Iterator iteracao = pilha_fila.iterator();
+
+        synchronized (iteracao) { // Bloquear o acesso desta lista por outros processos
+
+            while (iteracao.hasNext()) { // Em quanto tiver dados na lista irá processar//
+                ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next();
+
+                // Processar N notas fiscais
+                // Gerar N listas de PDF
+                // Gerar N envio de emails
+
+                System.out.println("----------------------------------------");
+
+                System.out.println(processar.getEmail());
+                System.out.println(processar.getNome());
+
+
+                iteracao.remove();
+
+                try {
+                    Thread.sleep(100); // Tempo para descarga de memória
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        try {
+            Thread.sleep(1000); // Para a limpeza de memória após o processamento da lista
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
