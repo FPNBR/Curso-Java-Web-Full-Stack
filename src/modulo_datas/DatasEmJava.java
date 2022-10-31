3720e5239839b1a4ddd58df73ae132da4fc6f22d
FPNBR <felipepny@gmail.com>
2022-10-31 19:07:31 -0300

    Cálculo de dia, ano e mês com Calendar

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -2,22 +2,26 @@ package modulo_datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
 public class DatasEmJava {
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = Calendar.getInstance(); // Pega a data atual
 
-        Date dataVencimentoBoleto = simpleDateFormat.parse("31/10/2022");
-        Date dataAtualHoje = simpleDateFormat.parse("31/10/2022");
+        // Simular que a data vem do banco de dados
 
-        // After - Se a data 1 é maior que a data 2, Before - Se a data 1 é menor que a data 2
-        if (dataVencimentoBoleto.before(dataAtualHoje)) {
-            System.out.println("Boleto vencido");
-        }
-        else {
-            System.out.println("Boleto não vencido");
-        }
+        calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2022")); // Definindo uma data qualquer
+
+        calendar.add(Calendar.DAY_OF_MONTH, -40); // Data de hoje - 40 dias
+
+        System.out.println("Subtraindo dia do mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+
+        calendar.add(Calendar.MONTH, 1); // Somando o mês
+
+        System.out.println("Somando o mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+
+        calendar.add(Calendar.YEAR, 1); // Somando o ano
+        System.out.println("Somando 1 ano: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
     }
 }
