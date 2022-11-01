e6eeb0e45542bf93e5be287f4a8c66f2594fe4e2
FPNBR <felipepny@gmail.com>
2022-11-01 00:52:43 -0300

    Gerando parcelas mensais e suas datas

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -1,15 +1,22 @@
 package modulo_datas;
 
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatasEmJava {
     public static void main(String[] args) throws ParseException {
 
-        long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-01-01"), LocalDate.now()); // Total de dias de uma data até hoje
+        Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2022"); // Data inicial
 
-        System.out.println("Possui " + dias + " dias entra a faixa de data");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dataInicial);
 
+        for (int parcela = 1; parcela <= 12; parcela ++) { // Gerar parcelas
+            calendar.add(Calendar.MONTH, 1); // A cada iteração adiciona um mês
+
+            System.out.println("Parcela número: " + parcela + " vencimento é em: " + new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+        }
     }
 }
