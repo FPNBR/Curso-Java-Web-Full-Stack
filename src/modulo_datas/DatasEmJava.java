a49b85fe52884116b5b5df2afc0d5dd26c2c4b0f
FPNBR <felipepny@gmail.com>
2022-11-01 00:41:37 -0300

    Faixa de tempo com ChronoUnit

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -1,27 +1,15 @@
 package modulo_datas;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class DatasEmJava {
     public static void main(String[] args) throws ParseException {
 
-        Calendar calendar = Calendar.getInstance(); // Pega a data atual
+        long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-01-01"), LocalDate.now()); // Total de dias de uma data até hoje
 
-        // Simular que a data vem do banco de dados
+        System.out.println("Possui " + dias + " dias entra a faixa de data");
 
-        calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2022")); // Definindo uma data qualquer
-
-        calendar.add(Calendar.DAY_OF_MONTH, -40); // Data de hoje - 40 dias
-
-        System.out.println("Subtraindo dia do mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
-
-        calendar.add(Calendar.MONTH, 1); // Somando o mês
-
-        System.out.println("Somando o mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
-
-        calendar.add(Calendar.YEAR, 1); // Somando o ano
-        System.out.println("Somando 1 ano: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
     }
 }
