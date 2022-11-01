f8172592ad672ad0c902e79cea3473694d417309
FPNBR <felipepny@gmail.com>
2022-11-01 01:33:23 -0300

    Java Time - Formatando data e hora

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava {
     public static void main(String[] args) throws ParseException {
@@ -12,14 +13,14 @@ public class DatasEmJava {
 
         LocalDate dataAtual = LocalDate.now();
 
-        System.out.println("Data atual: " + dataAtual);
+        System.out.println("Data atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
         LocalTime horaAtual = LocalTime.now();
 
-        System.out.println("Hora atual: " + horaAtual);
+        System.out.println("Hora atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
 
         LocalDateTime dataAtualHoraAtual = LocalDateTime.now();
 
-        System.out.println("Data e hora atual: " + dataAtualHoraAtual);
+        System.out.println("Data e hora atual: " + dataAtualHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
     }
 }
