d657442f100e681250b89599fe328a48650d9962
Felipe Pinheiro Nery <felipepny@gmail.com>
2022-11-06 01:17:59 -0300

    JavaMail - Configurando as propriedades de conexão

--- src/test/java/org/cursojava/enviando/email/AppTest.java
+++ src/test/java/org/cursojava/enviando/email/AppTest.java
@@ -2,10 +2,18 @@ package org.cursojava.enviando.email;
 
 import org.junit.Test;
 
+import java.util.Properties;
+
 public class AppTest {
 
     @Test
     public void testeEmail() {
-
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true"); // Autorização
+        properties.put("mail.smtp.starttls", "true"); // Autenticação
+        properties.put("mail.smtp.host", "smtp.gmail.com"); // Servidor Gmail Google
+        properties.put("mail.smtp.port", "465"); // Porta do servidor
+        properties.put("mail.smtp.socketFactory.port", "465"); // Especifica a porta a ser conectada pelo socket
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Classe socket de conexão ao SMTP
     }
 }
