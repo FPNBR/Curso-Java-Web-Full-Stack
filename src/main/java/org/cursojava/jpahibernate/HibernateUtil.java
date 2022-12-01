d1216a3dde9c7b9a2af02cf28e2c059c4a19e272
FPNBR <felipepny@gmail.com>
2022-12-01 00:06:54 -0300

    ORM com JPA e Hibernate - Criando a classe Java Hibernate Util

---
+++ src/main/java/org/cursojava/jpahibernate/HibernateUtil.java
@@ -0,0 +1,28 @@
+package org.cursojava.jpahibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+    public static EntityManagerFactory entityManagerFactory = null;
+
+    static {
+        init();
+    }
+
+    private static void init() {
+        try {
+            if (entityManagerFactory == null) {
+                entityManagerFactory = Persistence.createEntityManagerFactory("jpa"); // Pega as configurações da persistence.xml
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static EntityManager entityManager() {
+        return entityManagerFactory.createEntityManager(); // Fornece a parte de persistência
+    }
+}
---
+++ src/test/java/org/cursojava/jpahibernate/TesteHibernateUtil.java
@@ -0,0 +1,10 @@
+package org.cursojava.jpahibernate;
+
+import org.junit.Test;
+
+public class TesteHibernateUtil {
+    @Test
+    public void testeHibernateUtil() {
+        HibernateUtil.entityManager();
+    }
+}
