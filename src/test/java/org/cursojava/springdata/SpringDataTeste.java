dcfa7c7e824644e2c2531372c8cfb9a8c25d3da1
FPNBR <felipepny@gmail.com>
2023-03-13 09:52:23 -0300

    Sprint Data - Criando a classe de teste

---
+++ src/test/java/org/cursojava/springdata/SpringDataTeste.java
@@ -0,0 +1,15 @@
+package org.cursojava.springdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+public class SpringDataTeste {
+    @Test
+    public void testeInsert() {
+        System.out.println("Iniciou o spring com sucesso");
+    }
+}
